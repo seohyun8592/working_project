bad3739c84b65cc453a7e60769292d419600eca7
seohyun8592 <112323229+seohyun8592@users.noreply.github.com>
2022-09-16 23:57:32 +0900

    Add files via upload

--- js/ui.js
+++ js/ui.js
@@ -1,138 +1,229 @@
 'use strict';
-
+// string
 const ON = 'on';
 const ACTIVE = 'active';
+const SCROLL = 'scroll';
+const CLICK = 'click';
 
-const body = document.querySelector('body');
+// element
 const header = document.querySelector('header');
-const lyWrap = document.querySelector('.ly-wrap');
-const logo = document.querySelector('header .logo');
-const nav = document.querySelector('nav');
-const subGnb = document.querySelectorAll('.gnb__depth');
-const contWrap = document.querySelector('.cont-area .cont__wrap');
-const headerBtn = document.querySelector('.header__btn');
+const conMain = document.querySelector('.ly-contents__main');
+const conArea = document.querySelector('.contents-area');
+const dim = document.querySelectorAll('.dim');
 
-const conWrapCheck = contWrap.parentElement.previousElementSibling;
+// 헤더 고정
+function headerFixed() {
+  const checkPoint = conMain.getBoundingClientRect().top;
+  const headerH = header.offsetHeight;
 
-const headerH = header.offsetHeight;
-const navH = nav.offsetHeight;
-
-let checkClass;
-
-// 모바일 슬라이드 메뉴
-function slideMenuHandler() {
-  checkClass = headerBtn.classList.contains(ON);
-  if (checkClass) {
-    headerBtn.classList.remove(ON);
-    nav.style.right = `-100%`;
-    logo.style.zIndex = 1;
-    lyWrap.classList.remove('stop-scroll');
+  if (checkPoint < headerH) {
+    header.classList.add(ON);
   } else {
-    headerBtn.classList.add(ON);
-    nav.style.right = 0;
-    logo.style.zIndex = 999;
-    lyWrap.classList.add('stop-scroll');
+    header.classList.remove(ON);
   }
+}
+
+// 스킬
+function skillPerMove() {
+  const skillWrap = document.querySelector('.skill-item');
+  const skillPer = document.querySelectorAll('.skill__per');
+  const skillWrapScrT = skillWrap.getBoundingClientRect().top;
+  skillPer.forEach((el) => {
+    el.style.width = el.dataset.per + '%';
+    let scrT = window.scrollY;
+    if (scrT > skillWrapScrT) {
+      el.classList.add(ACTIVE);
+    }
+  });
 }
 
 // 탭
-function moveTabHandler() {
-  const tabBtn = document.querySelectorAll('.tab__btn .btn');
-  const tabCon = document.querySelectorAll('.tab__cont .cont');
+function tabEvtHandler() {
+  const tabArea = document.querySelectorAll('.tab-area');
 
-  tabBtn.forEach((el, idx) => {
-    el.addEventListener('click', () => {
-      tabBtn.forEach((e, i) => {
-        e.classList.remove(ON);
-        tabCon[i].classList.remove(ON);
+  tabArea.forEach((el, i) => {
+    const tabBtnWap = el.querySelector('.tab-area__btn');
+    const tabConWrap = el.querySelector('.tab-area__contents');
+    const tabBtn = tabBtnWap.querySelectorAll('.tab__btn');
+    tabBtn.forEach((btn, idx) => {
+      btn.addEventListener(CLICK, () => {
+        for (let z = 0; z < tabBtn.length; z++) {
+          tabBtn[z].parentElement.classList.remove(ON);
+          tabConWrap.children[z].classList.remove(ON);
+        }
+        btn.parentElement.classList.add(ON);
+        tabConWrap.children[idx].classList.add(ON);
       });
-      el.classList.add(ON);
-      tabCon[idx].classList.add(ON);
     });
   });
 }
-// 메뉴 드롭다운
-function navDropDown() {
-  checkClass = nav.classList.contains('mobile');
-  if (checkClass) {
+
+function onButtonClick(event) {
+  const items = document.querySelectorAll('.sort__contents .contents');
+  const key = event.target.dataset.type;
+  const arr = [];
+
+  if (!key === null) {
     return;
   }
-  let cofH = 0;
-  subGnb.forEach((el, i) => {
-    const subGnbH = el.offsetHeight;
 
-    if (cofH < subGnbH) {
-      cofH = subGnbH;
-    }
+  items.forEach((z) => {
+    const allBtn = document.querySelector('.sort__btn--all');
+    allBtn.addEventListener(CLICK, (event) => {
+      z.classList.add(ON);
+    });
+    z.classList.remove(ON);
 
-    if (header.offsetHeight === headerH) {
-      header.style.height = `${cofH + headerH}px`;
-      nav.style.height = `${cofH + headerH}px`;
-      header.classList.add(ACTIVE);
-      nav.classList.add(ON);
-    } else {
-      header.style.height = `${headerH}px`;
-      nav.style.height = `${navH}px`;
-      header.classList.remove(ACTIVE);
-      nav.classList.remove(ON);
-    }
+    arr.push(z);
+    const filtered = arr.filter((item) => item.dataset.type === key);
+
+    filtered.forEach((con) => {
+      console.log(z);
+      con.classList.add(ON);
+    });
   });
 }
 
-const deviceSizeHandler = {
-  type01: () => {
-    nav.classList.add('mobile');
-    nav.style.height = `100vh`;
-    if (conWrapCheck === null) {
-      return;
-    } else {
-      contWrap.classList.remove('cont__wrap--flex');
+function setEventListener() {
+  const sortBtn = document.querySelectorAll('.sort__btn');
+  sortBtn.forEach((el) => {
+    el.addEventListener(CLICK, (event) => {
+      sortBtn.forEach((e) => {
+        e.classList.remove(ON);
+      });
+      el.classList.add(ON);
+
+      onButtonClick(event);
+    });
+  });
+}
+setEventListener();
+
+function formSubmitData() {
+  const formSubmit = document.querySelector('.btn__submit');
+  const txtArea = document.querySelector('#txtArea');
+
+  if (!formSubmit) {
+    return;
+  }
+  formSubmit.addEventListener(CLICK, () => {
+    $.ajax({
+      type: 'GET',
+      url: 'https://script.google.com/macros/s/AKfycbw0E9u5MWN5eCPElu18pbJTAUDThNPzPG52qsfMQJtBgBeyzlkvMQriOILAr1v6G4m1/exec',
+      data: {
+        메세지: txtArea.value,
+      },
+      success: function (response) {
+        //값 비워주기
+        txtArea.value = '';
+        formOpenPopup();
+      },
+      error: function (request) {
+        console.log('error');
+      },
+    });
+  });
+}
+
+function formOpenPopup() {
+  dim.forEach((el) => {
+    if (el.id === 'pop01') {
+      el.classList.add(ON);
     }
+  });
+}
+function openPopUp(id) {
+  id.classList.add(ON);
+}
+
+function closePopUp(id) {
+  id.classList.remove(ON);
+}
+
+// visual
+const img = document.querySelector('.ly-contents__visual .img-area img');
+let attr = img.getAttribute('src');
+let seq_play = true;
+let _img_load = 0;
+let _img_count = 192;
+let idx = 0;
+
+function seq_init() {
+  for (idx = 0; idx <= _img_count; idx++) {
+    let _img_tmp = new Image();
+    _img_tmp.src = `images/sequence/sky${idx}.jpg`;
+    _img_tmp.onload = function () {
+      ++_img_load;
+      if (_img_load == _img_count) {
+        rolling();
+      }
+    };
+    // _img_tmp.onerror = function () {
+    //   ++_img_load;
+    //   if (_img_load == _img_count) {
+    //     rolling();
+    //   }
+    // };
+  }
+  idx = 0;
+  rolling();
+}
+function rolling() {
+  const set = setTimeout(function () {
+    if (seq_play) {
+      idx++;
+      img.setAttribute('src', `images/sequence/sky${idx}.jpg`);
+    }
+    if (idx == _img_count) {
+      seq_play = false;
+      idx = 0;
+    }
+    if (!seq_play) {
+      if (idx == 0) seq_play = true;
+    }
+    rolling();
+  }, 100);
+
+  rollingClear(set);
+}
+
+function rollingClear(set) {
+  let scrT = window.scrollY;
+  if (scrT > 0) {
+    clearTimeout(set);
+  }
+}
+
+seq_init();
+
+const bindEvt = {
+  scroll: function (el, func) {
+    el.addEventListener(SCROLL, () => {
+      func();
+    });
   },
-  type02: () => {
-    nav.classList.remove('mobile');
-    nav.style.height = `auto`;
-    if (conWrapCheck === null) {
-      return;
-    } else {
-      contWrap.classList.add('cont__wrap--flex');
-    }
+
+  click: function (el, func) {
+    el.addEventListener(CLICK, () => {
+      func();
+    });
   },
 };
 
-// 화면 리사이징
-function slideMenuResize() {
-  const winW = window.innerWidth;
-
-  if (winW <= 1024) {
-    deviceSizeHandler.type01();
-  } else {
-    deviceSizeHandler.type02();
-  }
+// 이벤트 연결
+function bindEvtHandler() {
+  bindEvt.scroll(window, headerFixed);
+  bindEvt.scroll(window, skillPerMove);
+  bindEvt.scroll(window, rollingClear);
 }
 
-// 디바이스 해상도 체크
-function ratioMatch() {
-  const ratio = window.matchMedia('(max-width: 1024px)');
-
-  if (ratio.matches) {
-    deviceSizeHandler.type01();
-  } else {
-    deviceSizeHandler.type02();
-  }
-}
-
-window.addEventListener('resize', slideMenuResize);
-nav.addEventListener('mouseenter', navDropDown);
-nav.addEventListener('mouseleave', navDropDown);
-
-if (headerBtn) {
-  headerBtn.addEventListener('click', slideMenuHandler);
-}
-
+// init
 function init() {
-  ratioMatch();
-  moveTabHandler();
+  bindEvtHandler();
+  tabEvtHandler();
+  formSubmitData();
 }
 
-window.addEventListener('DOMContentLoaded', init);
+document.addEventListener('DOMContentLoaded', function () {
+  init();
+});
