a95e1e217f77fba3f6574cc56ed01e36ed805769
김서현 <shhs0902@naver.com>
2022-10-21 09:17:54 +0900

    추가

--- sh_portfolio/js/ui.js
+++ sh_portfolio/js/ui.js
@@ -1,15 +1,16 @@
 'use strict';
 // string
 const ON = 'on';
+const ACTIVE = 'active';
 const SCROLL = 'scroll';
 const CLICK = 'click';
 
 // element
+const body = document.querySelector('body');
 const header = document.querySelector('header');
 const conMain = document.querySelector('.ly-contents__main');
+const conArea = document.querySelector('.contents-area');
 const dim = document.querySelectorAll('.dim');
-
-// const getItem = JSON.parse(localStorage.getItem('item'));
 
 // 헤더 고정
 function headerFixed() {
@@ -23,6 +24,20 @@ function headerFixed() {
   }
 }
 
+// 스킬
+function skillPerMove() {
+  const skillWrap = document.querySelector('.skill-item');
+  const skillPer = document.querySelectorAll('.skill__per');
+  const skillWrapScrT = skillWrap.getBoundingClientRect().top;
+  skillPer.forEach((el) => {
+    el.style.width = el.dataset.per + '%';
+    let scrT = window.scrollY;
+    if (scrT > skillWrapScrT) {
+      el.classList.add(ACTIVE);
+    }
+  });
+}
+
 // 탭
 function tabEvtHandler() {
   const tabArea = document.querySelectorAll('.tab-area');
@@ -33,7 +48,6 @@ function tabEvtHandler() {
     const tabBtn = tabBtnWap.querySelectorAll('.tab__btn');
     tabBtn.forEach((btn, idx) => {
       btn.addEventListener(CLICK, () => {
-        console.log(el);
         for (let z = 0; z < tabBtn.length; z++) {
           tabBtn[z].parentElement.classList.remove(ON);
           tabConWrap.children[z].classList.remove(ON);
@@ -45,79 +59,49 @@ function tabEvtHandler() {
   });
 }
 
-function loadItem() {
-  return fetch('../data/item.json') //
-    .then((response) => response.json())
-    .then((json) => json.items);
-}
+function onButtonClick(event, z) {
+  const key = event.target.dataset.type;
+  const arr = [];
 
-function displayItems(items) {
-  const sorContainer = document.querySelector('.sort-area__contents');
-  sorContainer.innerHTML = items.map((item) => createHtmlString(item)).join('');
-}
-
-function createHtmlString(item) {
-  return `
-        <div class="sort__contents" data-value= "${item.type}">
-            <div class="img-area">
-              <img src="${item.img}" alt="${item.title}" />
-            </div>
-            <h4 class="title">${item.title}</h4>
-            <a
-              href="${item.src}"
-              target="_blank"
-            ></a>
-        </div>
-    `;
-}
-
-function onButtonClick(event, items) {
-  const dataset = event.target.dataset;
-  const key = dataset.type;
-  const value = dataset.value;
-
-  if (!key === null || !value === null) {
+  if (!key === null) {
     return;
   }
 
-  const filtered = items.filter((item) => item[key] === value);
-  displayItems(filtered);
-  // updateItem(items, key, value);
+  event.target.classList.add(ON);
+
+  z.classList.remove(ON);
+
+  arr.push(z);
+  const filtered = arr.filter((item) => item.dataset.type === key);
+
+  filtered.forEach((con) => {
+    con.classList.add(ON);
+  });
 }
 
-// function updateItem(items, key, value) {
-//   items.filter((item) => {
-//     if (item[key] === value) {
-//       item.classList.add('visivle');
-//     } else {
-//       item.classList.remove('visivle');
-//     }
-//   });
-// }
-
-function setEventListener(items) {
-  console.log(items);
+function setEventListener(z) {
+  const allBtn = document.querySelector('.sort__btn--all');
   const sortBtn = document.querySelectorAll('.sort__btn');
 
   sortBtn.forEach((el) => {
-    const allBtn = el.classList.contains('sort__btn--all');
+    allBtn.addEventListener(CLICK, () => {
+      allBtn.classList.add(ON);
+      z.classList.add(ON);
+      el.classList.remove(ON);
+    });
 
-    el.addEventListener(CLICK, (event) => {
+    el.addEventListener(CLICK, () => {
       sortBtn.forEach((e) => {
         e.classList.remove(ON);
+        allBtn.classList.remove(ON);
       });
-      el.classList.add(ON);
-
-      if (allBtn) {
-        displayItems(items);
-      } else {
-        onButtonClick(event, items);
-      }
+      onButtonClick(event, z);
     });
   });
 }
+
 function formSubmitData() {
-  const formSubmit = document.querySelector('.form-submit');
+  const formSubmit = document.querySelector('.btn__submit');
   const txtArea = document.querySelector('#txtArea');
 
   if (!formSubmit) {
@@ -157,6 +141,96 @@ function closePopUp(id) {
   id.classList.remove(ON);
 }
 
+// visual
+const img = document.querySelector('.ly-contents__visual .img-area img');
+let attr = img.getAttribute('src');
+let seq_play = true;
+let _img_load = 0;
+let _img_count = 192;
+let idx = 0;
+
+function fadeUp() {
+  const visual = document.querySelector('.ly-contents__visual');
+  const realVisual = document.querySelector('.visual__real');
+  const visualTxt = document.querySelector('.ly-contents__visual .txt-area');
+
+  let percent = 20;
+  let curIdx = 1;
+
+  setTimeout(() => {
+    const widthTranse = setInterval(() => {
+      let currentWToPer = percent + curIdx;
+      realVisual.style.width = `${currentWToPer}%`;
+      realVisual.style.height = `${currentWToPer}%`;
+
+      curIdx++;
+
+      visual.classList.add(ON);
+
+      if (visual.classList.contains(ON)) {
+        setTimeout(() => {
+          visualTxt.style.opacity = 1;
+        }, 1000);
+      }
+
+      if (currentWToPer === 100) {
+        clearInterval(widthTranse);
+        body.classList.remove('stop__scroll');
+      }
+    }, 10);
+  }, 2400);
+}
+// fadeUp();
+
+function seq_init() {
+  for (idx = 0; idx <= _img_count; idx++) {
+    let _img_tmp = new Image();
+    _img_tmp.src = `images/sequence/sky${idx}.jpg`;
+    _img_tmp.onload = function () {
+      ++_img_load;
+
+      if (_img_load === _img_count) {
+        fadeUp();
+
+        // rolling();
+      }
+    };
+    // _img_tmp.onerror = function () {
+    //   ++_img_load;
+    //   if (_img_load == _img_count) {
+    //     rolling();
+    //   }
+    // };
+  }
+  idx = 0;
+  rolling();
+}
+function rolling() {
+  const set = setTimeout(function () {
+    if (seq_play) {
+      idx++;
+      img.setAttribute('src', `images/sequence/sky${idx}.jpg`);
+    }
+    if (idx == _img_count) {
+      seq_play = false;
+      idx = 0;
+    }
+    if (!seq_play) {
+      if (idx == 0) seq_play = true;
+    }
+    rolling();
+  }, 100);
+
+  rollingClear(set);
+}
+
+function rollingClear(set) {
+  let scrT = window.scrollY;
+  if (scrT > 0) {
+    clearTimeout(set);
+  }
+}
+
 const bindEvt = {
   scroll: function (el, func) {
     el.addEventListener(SCROLL, () => {
@@ -170,21 +244,31 @@ const bindEvt = {
     });
   },
 };
-
+window.onload = function () {
+  setTimeout(() => {
+    scrollTo(0, 0);
+  }, 1);
+};
 // 이벤트 연결
-function bindEvtHandler(el) {
+function bindEvtHandler() {
   bindEvt.scroll(window, headerFixed);
+  bindEvt.scroll(window, skillPerMove);
+  bindEvt.scroll(window, rollingClear);
+
+  const items = document.querySelectorAll('.sort__contents .contents');
+
+  items.forEach((z) => {
+    z.classList.add(ON);
+    setEventListener(z);
+  });
 }
 
 // init
 function init() {
+  seq_init();
+  bindEvtHandler();
   tabEvtHandler();
   formSubmitData();
-  loadItem() //
-    .then((items) => {
-      displayItems(items);
-      setEventListener(items);
-    });
 }
 
 document.addEventListener('DOMContentLoaded', function () {
